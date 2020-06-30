9d77e2466277764c8c8a05de4af0e1c4ebbdfaf2
jason <jackobleed@gmail.com>
2020-07-01 00:04:21 +0100

    initial

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const mongo = require('mongodb').MongoClient;
 const client = require('socket.io').listen(4000).sockets;
 
-const uri = "mongodb+srv://HamidaBatista:TrayWay@cp23.zlhhc.mongodb.net/mongochat?retryWrites=true&w=majority";
-
-
+const uri = "mongodb+srv://HamidaBatista:TrayWay@cp23.zlhhc.mongodb.net/mongochat?retryWrites=true&w=majority";;
 //const clientt = new mongo(uri, { useNewUrlParser: true });
 
 
